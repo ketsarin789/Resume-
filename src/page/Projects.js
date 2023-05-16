fe09ad729a2b2e10c54ef5a4bdfc9078d680c6f8
Ketsarin Kayak <ketsarinkayak@Ketsarins-MacBook-Pro.local>
2023-05-16 11:51:32 -0700

    Add exchange rate

--- src/page/Projects.js
+++ src/page/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 import img from '../assets/img/projects/padthai.jpeg';
 import img1 from '../assets/img/projects/bobatea.jpeg';
 import img2 from '../assets/img/projects/project1.jpeg';
-
+import img3 from '../assets/img/projects/exchang.webp';
 import {Card, CardImg} from 'reactstrap';
 
 export const Projects = () => {
@@ -23,6 +23,11 @@ export const Projects = () => {
                 <Card className='p-3' style={{ backgroundColor: '#282c34' }} >
                         <CardImg src={img2} alt="" style={{width: 300}}/>
                         <a style={{padding: 10, color: "white"}} href='https://github.com/ketsarin789/Thai-Jelly-Art'>Thai Art Jelly Store</a>
+                    
+                </Card>
+                <Card className='p-3' style={{ backgroundColor: '#282c34' }} >
+                        <CardImg src={img3} alt="" style={{width: 300, height: 300}}/>
+                        <a style={{padding: 10, color: "white"}} href='https://main--effortless-tanuki-df551a.netlify.app/'>Exchange Rate --Live</a>
                     
                 </Card>
                 
